24a8895a7c1f0cadddd803ac9f10244630ec432a
Xavier Lonjon <xavierlonjon@yahoo.fr>
2023-07-11 08:01:10 +0200

    3 optimised algo

--- main.py
+++ main.py
@@ -10,45 +10,93 @@ def load_csv(file):
     """Load the actions from the CSV file"""
     actions = []
     actions_knapsack = []
-    with open(file + '.csv', 'r') as file:
+    with open(file + ".csv", "r") as file:
         reader = csv.reader(file)
         next(reader)  # Skip the header row
         for row in reader:
-            if float(row[1]) > 0 : 
+            if float(row[1]) > 0:
                 action = {
-                    'action': row[0],
-                    'coût_par_action': int(float(row[1]) * 100),
-                    'bénéfice_après_2_ans': int(round(float(row[1]) * (float(row[2]) / 100), 2))
+                    "action": row[0],
+                    "coût_par_action": int(float(row[1]) * 100),
+                    "bénéfice_après_2_ans": int(
+                        round(float(row[1]) * (float(row[2]) / 100), 2)
+                    ),
                 }
                 actions_knapsack.append(action)
                 action = {
-                        'action': row[0],
-                        'coût_par_action': float(row[1]),
-                        'bénéfice_après_2_ans': float(row[2])
-                        }
+                    "action": row[0],
+                    "coût_par_action": float(row[1]),
+                    "bénéfice_après_2_ans": float(row[2]),
+                }
                 actions.append(action)
 
     return actions, actions_knapsack
 
 
-def write_to_csv(filename, data):
-    with open(filename, 'w', newline='') as file:
+def write_to_csv(filename, result1, result2, result3):
+    with open(filename, "w", newline="") as file:
         writer = csv.writer(file)
+
+        best_combination_to_csv(writer,result1)
+        best_combination_to_csv(writer,result2)
+        best_combination_to_csv(writer,result3)
+
+def best_combination_to_csv(writer, data):
         writer.writerow(["Action", "Cost", "Benefit", "Total benefit", "Total cost"])
-        writer.writerow(["", "", "", data[0]],"")
+        writer.writerow(["", "", "", data[0], ""])
         writer.writerow(["", "", "", "", data[2]])
         for action in data[1]:
-            writer.writerow([action['action'], action['coût_par_action'], action['bénéfice_après_2_ans']])
+            writer.writerow(
+                [
+                    action["action"],
+                    action["coût_par_action"],
+                    action["bénéfice_après_2_ans"],
+                    "",
+                    ""
+                ]
+            )
 
-#optimised(load_csv("actions"))
-#bruteforce(load_csv("actions"))
-data1 = load_csv("dataset1_Python+P7")
+def main():
+    data1 = load_csv("dataset1_Python+P7")
+    data2 = load_csv("dataset2_Python+P7")
+    result1 = Sorted_method(data1[0])
+    result2 = Simple_knapsack_method(data1[1])
+    result3 = Knapsack_method(data1[1])
+    write_to_csv("result_data1.csv",result1, result2, result3)
+
+
+
+main()
+
+"""data1 = load_csv("dataset1_Python+P7")
 data2 = load_csv("dataset2_Python+P7")
-result =Sorted_method(data1[0])
-write_to_csv("sorted", result)
+result = Sorted_method(data1[0])
+print("debug1")
+write_to_csv("result_data1.csv", result)
+print("debug2")
+result = Simple_knapsack_method(data1[1])
+print("debug3")
+write_to_csv("result_data1.csv", result)
+print("debug4")
+result = Knapsack_method(data1[1])
+print("debug")
+write_to_csv("result_data1.csv", result)"""
+
+
+
+# optimised(load_csv("actions"))
+# data = load_csv("actions")
+# bruteforce(data[0])
+
+# result = Sorted_method(data1[0])
+# write_to_csv("sorted", result)
+# result1 = Knapsack_method(data1[1])
+# write_to_csv("Knapsack_method2", result1)
+# result2 = Knapsack_method(data2[1])
+# write_to_csv("Knapsack_method2", result2)
+#Sorted_method(data1[0])
 #Sorted_method(data2[0])
 #Simple_knapsack_method(data2[1])
 #Simple_knapsack_method(data1[1])
 #Knapsack_method(data1[1])
 #Knapsack_method(data2[1])
-
--- optimised.py
+++ optimised.py
@@ -4,80 +4,91 @@ from decorators import performance_decorator
 @performance_decorator
 def Sorted_method(actions):
     """Sort the actions by descending order of benefit"""
-    sorted_actions = sorted(actions, key=lambda x: x['bénéfice_après_2_ans'], reverse=True)
+    sorted_actions = sorted(
+        actions, key=lambda x: x["bénéfice_après_2_ans"], reverse=True
+    )
     # Initialize variables
     budget_remaining = 500
     actions_purchased = []
 
     # Iterate through the sorted actions
     for action in sorted_actions:
-        if action['coût_par_action'] <= budget_remaining:
+        if action["coût_par_action"] <= budget_remaining:
             actions_purchased.append(action)
-            budget_remaining -= action['coût_par_action']
+            budget_remaining -= action["coût_par_action"]
 
     # Display the purchased actions
     total_purchase = 0
     total_benefit = 0
     for action in actions_purchased:
-        benefit = action['coût_par_action']*action['bénéfice_après_2_ans']/100
-        total_purchase = total_purchase + action['coût_par_action']
+        benefit = action["coût_par_action"] * action["bénéfice_après_2_ans"] / 100
+        total_purchase = total_purchase + action["coût_par_action"]
         total_benefit = total_benefit + benefit
-        print(f"Purchased: {action['action']} (Cost: {action['coût_par_action']} euros, Benefit: {action['bénéfice_après_2_ans']})")
+        print(
+            f"Purchased: {action['action']} (Cost: {action['coût_par_action']} euros, Benefit: {action['bénéfice_après_2_ans']})"
+        )
 
     print(total_benefit)
     print(total_purchase)
-    return total_benefit,actions_purchased, total_purchase
+    return total_benefit, actions_purchased, total_purchase
+
 
 @performance_decorator
-def Knapsack_method(actions):
+def Knapsack_method2(actions):
     """Knapsack Problem with Dynamic Memory Allocation"""
-    #sorted(actions, key=lambda x: x['bénéfice_après_2_ans'], reverse=True)
     # Initialize variables
     max_cost = 50000
     actions_purchased = []
-    
+
     # Create a 2D array to store the maximum benefit for each budget and number of actions
     dp = [[0] * (max_cost + 1) for _ in range(len(actions) + 1)]
-    
+
     # Iterate through the sorted actions
     for i in range(1, len(actions) + 1):
         for j in range(1, max_cost + 1):
             action = actions[i - 1]
-            if action['coût_par_action'] <= j:
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - action['coût_par_action']] + action['bénéfice_après_2_ans'])
+            if action["coût_par_action"] <= j:
+                dp[i][j] = max(
+                    dp[i - 1][j],
+                    dp[i - 1][j - action["coût_par_action"]]
+                    + action["bénéfice_après_2_ans"],
+                )
             else:
                 dp[i][j] = dp[i - 1][j]
-    
+
     # Find the actions to purchase
     j = max_cost
     for i in range(len(actions), 0, -1):
         if dp[i][j] != dp[i - 1][j]:
             action = actions[i - 1]
             actions_purchased.append(action)
-            j -= action['coût_par_action']
-    
+            j -= action["coût_par_action"]
+
     # Display the purchased actions
     total_purchase = 0
     total_benefit = 0
     for action in actions_purchased:
-        benefit = action['bénéfice_après_2_ans']
-        total_purchase += action['coût_par_action']
-        total_benefit += action['bénéfice_après_2_ans']
-        print(f"Purchased: {action['action']} (Cost: {action['coût_par_action']/100} euros, Benefit: {benefit})")
-    
+        benefit = action["bénéfice_après_2_ans"]
+        total_purchase += action["coût_par_action"]
+        total_benefit += action["bénéfice_après_2_ans"]
+        print(
+            f"Purchased: {action['action']} (Cost: {action['coût_par_action']/100} euros, Benefit: {benefit})"
+        )
+
     print(total_benefit)
-    print(total_purchase/100)
-    return total_benefit,actions_purchased, total_purchase
+    print(total_purchase / 100)
+    return total_benefit, actions_purchased, total_purchase/100
+
 
 @performance_decorator
 def Simple_knapsack_method(actions):
-    max_cost=50000
+    max_cost = 50000
     n = len(actions)
     dp = [[0] * (max_cost + 1) for _ in range(n + 1)]
 
     for i in range(1, n + 1):
-        cost = actions[i - 1]['coût_par_action']
-        benefit = actions[i - 1]['bénéfice_après_2_ans']
+        cost = actions[i - 1]["coût_par_action"]
+        benefit = actions[i - 1]["bénéfice_après_2_ans"]
         for j in range(1, max_cost + 1):
             if cost <= j:
                 dp[i][j] = max(benefit + dp[i - 1][j - cost], dp[i - 1][j])
@@ -91,17 +102,51 @@ def Simple_knapsack_method(actions):
     for i in range(n, 0, -1):
         if dp[i][j] != dp[i - 1][j]:
             actions_purchased.append(actions[i - 1])
-            j -= actions[i - 1]['coût_par_action']
+            j -= actions[i - 1]["coût_par_action"]
 
     # Display the purchased actions
     total_purchase = 0
     total_benefit = 0
     for action in actions_purchased:
-        benefit = action['bénéfice_après_2_ans']
-        total_purchase += action['coût_par_action']
-        total_benefit += action['bénéfice_après_2_ans']
-        print(f"Purchased: {action['action']} (Cost: {action['coût_par_action']/100} euros, Benefit: {benefit})")
-    
+        benefit = action["bénéfice_après_2_ans"]
+        total_purchase += action["coût_par_action"]
+        total_benefit += action["bénéfice_après_2_ans"]
+        print(
+            f"Purchased: {action['action']} (Cost: {action['coût_par_action']/100} euros, Benefit: {benefit})"
+        )
+
     print(total_benefit)
-    print(total_purchase/100)
-    return total_benefit,actions_purchased,total_purchase
+    print(total_purchase / 100)
+    return total_benefit, actions_purchased, total_purchase/100
+
+
+@performance_decorator
+def Knapsack_method(actions):
+    """Knapsack Problem with Dynamic Memory Allocation"""
+    max_cost = 50000
+    actions_purchased = []
+
+    dp = [0] * (max_cost + 1)
+
+    for action in actions:
+        for j in range(max_cost, action["coût_par_action"] - 1, -1):
+            dp[j] = max(dp[j], dp[j - action["coût_par_action"]] + action["bénéfice_après_2_ans"])
+
+    j = max_cost
+    for action in reversed(actions):
+        if j - action["coût_par_action"] >= 0 and j - action["coût_par_action"] < len(dp):
+            if dp[j] != dp[j - action["coût_par_action"]]:
+                actions_purchased.append(action)
+                j -= action["coût_par_action"]
+
+    total_purchase = sum(action["coût_par_action"] for action in actions_purchased)
+    total_benefit = sum(action["bénéfice_après_2_ans"] for action in actions_purchased)
+
+    for action in actions_purchased:
+        print(
+            f"Purchased: {action['action']} (Cost: {action['coût_par_action']/100} euros, Benefit: {action['bénéfice_après_2_ans']})"
+        )
+
+    print(total_benefit)
+    print(total_purchase / 100)
+    return total_benefit, actions_purchased, total_purchase/100
